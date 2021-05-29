27a6580e1aa10c49ab6b73434f761221cfcf9c36
linpicio <3127966867@qq.com>
2021-05-30 01:36:47 +0800

    feat: 微调爱发电按钮样式

--- src/pages/pixiv/novel/[id]/components/AfdianButton.tsx
+++ src/pages/pixiv/novel/[id]/components/AfdianButton.tsx
@@ -6,9 +6,9 @@ interface AfdianButtonProps {
 
 const AfdianButton: React.FC<AfdianButtonProps> = ({ url }) => {
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center my-4">
       <div
-        className="mb-4 bg-purple-500 rounded-full w-32 h-32 flex items-center justify-center"
+        className="mb-4 bg-purple-500 rounded-full w-28 h-28 flex items-center justify-center"
         style={{
           boxShadow: '0 6px 24px #777',
         }}
@@ -16,8 +16,8 @@ const AfdianButton: React.FC<AfdianButtonProps> = ({ url }) => {
       >
         <img src={AfdianImg} />
       </div>
-      <div className="mb-1 text-3xl font-bold">支持作者</div>
-      <div className="text-purple-500 text-lg">爱发电赞助</div>
+      <div className="mb-1 text-2xl font-bold leading-5">支持作者</div>
+      <div className="text-purple-500 text-base">爱发电赞助</div>
     </div>
   );
 };
