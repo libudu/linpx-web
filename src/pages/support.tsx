95f234699875e98d9b65167afa555f9171a073c2
linpicio <3127966867@qq.com>
2021-08-07 22:36:52 +0800

    feat: 优化赞助界面

--- src/pages/support.tsx
+++ src/pages/support.tsx
@@ -1,29 +1,30 @@
 import AfdianImg from '@/assets/icon/afdian.png';
+import BlackLogoPng from '@/assets/logo/black_logo.png';
+import WordLogoPng from '@/assets/logo/word_logo.png';
 
 export default function IndexPage() {
   return (
     <div className="text-center py-12">
-      <div className="text-2xl">LINPX的爱发电</div>
-      <div className="text-xs mt-1 opacity-60">
-        服务器运维…开发成本…无料周边…
+      <div className="flex flex-col items-center text-base">
+        <img className="w-20" src={BlackLogoPng}></img>
+        <img className="w-28 mr-2 mb-1 mt-2" src={WordLogoPng}></img>
+        <div className="text-xl">LINPX IS NOT PIXIV</div>
       </div>
-      <div className="flex justify-center mt-4 mb-2">
+      <div className="flex justify-center mt-16 mb-2">
         <div
-          className="mb-4 bg-purple-500 rounded-full w-28 h-28 flex items-center justify-center"
-          style={{
-            boxShadow: '0 6px 24px #777',
-          }}
+          className="mb-4 bg-purple-500 rounded-full w-24 h-24 flex items-center justify-center"
           onClick={() => window.open('https://afdian.net/@LINPX')}
         >
           <img src={AfdianImg} />
         </div>
       </div>
-      <div className="text-sm opacity-80">
-        <div>所有赞助者将被</div>
-        <div>记载于开源地址的readme中</div>
-      </div>
-      <div className="text-xs line-through opacity-25">
-        设计师先躺了，所以先程序糊一下后面再找设计要稿子做更好看的
+      <div className="font-bold text-3xl">赞助我们</div>
+      <div className="text-lg opacity-80 mt-2">
+        <div>但赞助的并不是我们</div>
+        <div>爱发电款项与本网站无关</div>
+        <div>就当是在😄在💧筹</div>
+        <div>我们会将您的名字记录在</div>
+        <div>网站开源项目的readme中</div>
       </div>
     </div>
   );
