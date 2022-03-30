145d7502ebc3912f99e3e2958fab149186949b01
libudu <823446473@qq.com>
2022-03-30 23:45:28 +0800

    feat: 完成了排序选项的视觉优化

--- src/pages/post/index.tsx
+++ src/pages/post/index.tsx
@@ -1,14 +1,16 @@
-import PageLayout from '@/components/PageLayout';
-import TopImg from '@/assets/icon/top.png';
-import { Dropdown, Pagination } from 'antd';
-import { IPost, postApi, usePixivNovelProfiles } from '@/api';
-import { history } from 'umi';
 import { useState } from 'react';
-import NameTime from './components/NameTime';
-import { Array2Map } from '@/types';
-import NovelRefer from './components/NovelRefer';
+import { history } from 'umi';
+import { Dropdown, Pagination } from 'antd';
 import { CaretDownOutlined } from '@ant-design/icons';
+
+import { IPost, postApi, usePixivNovelProfiles } from '@/api';
+import { Array2Map } from '@/types';
+import PageLayout from '@/components/PageLayout';
+import NameTime from './components/NameTime';
+import NovelRefer from './components/NovelRefer';
+
 import LinpxNewImg from '@/assets/icon/linpx_new.png';
+import TopImg from '@/assets/icon/top.png';
 
 const topPostList = [
   {
@@ -47,71 +49,108 @@ const TopPostElement = (
 );
 
 // 普通帖子
-const PostPreviewElement: React.FC<{ postList: IPost[]; sortType: ISortType }> =
-  ({ postList, sortType }) => {
-    const referList = postList
-      .map(({ refer }) => refer)
-      .filter((refer) => refer && refer.type && refer.data);
-    const referNovelList = referList
-      .filter((refer) => refer?.type == 'novel')
-      .map((refer) => refer?.data) as string[];
-    const novelInfo = usePixivNovelProfiles(referNovelList);
-    const novelInfoMap = novelInfo ? Array2Map(novelInfo) : {};
-    return (
-      <>
-        {postList.map(
-          ({
-            ip,
-            title,
-            content,
-            _time,
-            id,
-            commentCount,
-            refer,
-            createTime,
-          }) => {
-            let referElement = null;
-            // 引用小说
-            if (refer?.type == 'novel' && refer.data) {
-              const novelInfo = novelInfoMap[refer.data];
-              if (novelInfo) {
-                referElement = <NovelRefer {...novelInfo} />;
-              }
+const PostPreviewElement: React.FC<{
+  postList: IPost[];
+  timeType: 'post' | 'comment';
+}> = ({ postList, timeType }) => {
+  const referList = postList
+    .map(({ refer }) => refer)
+    .filter((refer) => refer && refer.type && refer.data);
+  const referNovelList = referList
+    .filter((refer) => refer?.type == 'novel')
+    .map((refer) => refer?.data) as string[];
+  const novelInfo = usePixivNovelProfiles(referNovelList);
+  const novelInfoMap = novelInfo ? Array2Map(novelInfo) : {};
+  return (
+    <>
+      {postList.map(
+        ({
+          ip,
+          title,
+          content,
+          _time,
+          id,
+          commentCount,
+          refer,
+          createTime,
+        }) => {
+          let referElement = null;
+          // 引用小说
+          if (refer?.type == 'novel' && refer.data) {
+            const novelInfo = novelInfoMap[refer.data];
+            if (novelInfo) {
+              referElement = <NovelRefer {...novelInfo} />;
             }
-            return (
-              <div
-                key={id}
-                className="px-4 py-2 hover:bg-gray-100 transition-all cursor-pointer"
-                style={{ borderBottom: '1px solid #ddd' }}
-                onClick={() => {
-                  history.push('/post/' + id);
-                }}
-              >
-                <NameTime
-                  ip={ip}
-                  _time={sortType == 'post' ? createTime : _time}
-                  rightEle={'回复: ' + commentCount}
-                />
-                <div className="u-line-1 font-bold mt-0.5">{title}</div>
-                <div className="u-line-2 text-base">{content}</div>
-                {referElement}
-              </div>
-            );
-          },
-        )}
-      </>
-    );
-  };
+          }
+          return (
+            <div
+              key={id}
+              className="px-4 py-2 hover:bg-gray-100 transition-all cursor-pointer"
+              style={{ borderBottom: '1px solid #ddd' }}
+              onClick={() => {
+                history.push('/post/' + id);
+              }}
+            >
+              <NameTime
+                ip={ip}
+                _time={timeType == 'post' ? createTime : _time}
+                rightEle={'回复: ' + commentCount}
+              />
+              <div className="u-line-1 font-bold mt-0.5">{title}</div>
+              <div className="u-line-2 text-base">{content}</div>
+              {referElement}
+            </div>
+          );
+        },
+      )}
+    </>
+  );
+};
 
 // 按事件排序
 // 帖子分区
 // todo:后端限制，每分钟最多发2条新帖，每小时最多发5条，每天最多发10条帖子
 const sortTypeMap = {
-  comment: '回复时间排序',
-  post: '发帖时间排序',
+  comment: '回复时间',
+  post: '发帖时间',
+  commentNum: '帖子热度',
 };
 
 type ISortType = keyof typeof sortTypeMap;
+
+const SortTypeDropdown: React.FC<{
+  sortType: ISortType;
+  setSortType: (sortType: ISortType) => void;
+}> = ({ sortType, setSortType }) => {
+  return (
+    <Dropdown
+      placement="bottomCenter"
+      trigger={['click']}
+      overlay={
+        <div
+          className="text-lg lp-bgcolor rounded-md overflow-hidden"
+          style={{ boxShadow: '0 0 4px #999' }}
+        >
+          {Object.entries(sortTypeMap).map(([sortType, text]) => (
+            <div
+              className="hover:bg-yellow-500 hover:bg-opacity-30 px-4"
+              style={{ borderBottom: '1px solid #eee' }}
+              key={sortType}
+              onClick={() => setSortType(sortType as any)}
+            >
+              {text}
+            </div>
+          ))}
+        </div>
+      }
+    >
+      <div className="pl-2">
+        {sortTypeMap[sortType]}
+        <CaretDownOutlined />
+      </div>
+    </Dropdown>
+  );
+};
 
 export default function () {
   const [page, setPage] = useState(1);
@@ -135,30 +174,12 @@ export default function () {
         style={{ borderBottom: '1px solid #ddd' }}
       >
         <div>帖子总数：{total}</div>
-        <Dropdown
-          placement="bottomCenter"
-          trigger={['click']}
-          overlay={
-            <div className="text-lg bg-gray-300 rounded-md py-1 overflow-hidden">
-              {Object.entries(sortTypeMap).map(([sortType, text]) => (
-                <div
-                  className="hover:bg-gray-400 px-2"
-                  key={sortType}
-                  onClick={() => setSortType(sortType as any)}
-                >
-                  {text}
-                </div>
-              ))}
-            </div>
-          }
-        >
-          <div>
-            {sortTypeMap[sortType]}
-            <CaretDownOutlined />
-          </div>
-        </Dropdown>
+        <SortTypeDropdown sortType={sortType} setSortType={setSortType} />
       </div>
-      <PostPreviewElement postList={postList} sortType={sortType} />
+      <PostPreviewElement
+        postList={postList}
+        timeType={sortType == 'post' ? 'post' : 'comment'}
+      />
       {
         // 两页以上显示分页器
         pageTotal > 1 && (
