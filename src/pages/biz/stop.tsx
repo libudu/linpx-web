fe63462de9601daabe4b0d51310c875fa8a57157
libudu <823446473@qq.com>
2022-12-05 23:28:23 +0800

    feat: 完成了停服公告和定时弹框

---
+++ src/pages/biz/stop.tsx
@@ -0,0 +1,55 @@
+import { useEffect } from 'react';
+import { openInfoModal } from '../components/Modal';
+
+export const isAfterStop = () => {
+  const end = new Date('2022-12-6 11:11:11');
+  const now = new Date();
+  if (now > end) {
+    return true;
+  }
+  return false;
+};
+
+export const checkStop = () => {
+  const start = new Date('2022-12-6 1:1:1');
+  const end = new Date('2022-12-6 11:11:11');
+  const now = new Date();
+  if (
+    location.host == 'linpx.linpicio.com' ||
+    location.host == 'furrynovel.xyz'
+  ) {
+    if (now > start && now < end) {
+      return true;
+    }
+  }
+  return false;
+};
+
+export const openStopModal = () => {
+  openInfoModal({
+    title: '《林匹克斯》12月6日维护通知',
+    children: (
+      <div className="text-sm text-left">
+        <div>亲爱的猫狗狼熊狐獭龙们，</div>
+        <div className="mt-2"></div>
+        <div>
+          自12月6日01:01:01起《林匹克斯》将进行临时维护，维护期间无法正常浏览网页，预计12月6日11:11:11后恢复。
+        </div>
+        <div className="mt-2"></div>
+        <div>爱是最伟大的魔法🕯️</div>
+        <div className="mt-2"></div>
+        <div>《林匹克斯》官方运营猫</div>
+        <div>2022年12月5日</div>
+      </div>
+    ),
+  });
+};
+
+const Stop = () => {
+  useEffect(() => {
+    openStopModal();
+  }, []);
+  return <div />;
+};
+
+export default Stop;
